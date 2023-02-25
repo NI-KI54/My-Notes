2665ef92577c666fb1c6105d62e817e7a4eae31f
NI-KI54 <nickmasih575@gmail.com>
2023-02-25 22:20:02 +0530

    added an object to get different id and content

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const btnEl = document.getElementById("btn");
 
+function createNoteEl(id, content){
+    console.log(id,content);
+};
 
 function addnote(){
-    console.log("clicked");
+    const noteobj = {
+        id:Math.floor(Math.random() * 100000),
+        content: "",
+    };
+    const noteEl = createNoteEl(noteobj.id, noteobj.content);
 }
 
 btnEl.addEventListener("click", addnote);
